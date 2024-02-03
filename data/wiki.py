6cdbe4421afa4cd08e36229cd8efc499838f8094
Neelan Patel <neelan@Neelans-MacBook-Pro.local>
2024-02-03 17:03:28 -0500

    fixed img size

--- data/wiki.py
+++ data/wiki.py
@@ -1,6 +1,8 @@
 import wikipedia
 from bs4 import BeautifulSoup
 import requests
+import re
+
 
 places = ["St. Augustine's University", "John Chavis Memorial Park", "Pope House Museum", "Estey Hall at Shaw University", "African American Civil War Memorial and Museum", "Frederick Douglass National Historic Site", "Martin Luther King, Jr. Memorial", "National Museum of African American History and Culture (NMAAHC)", "Howard Theatre"]
 data = {}
@@ -20,11 +22,15 @@ url = getURL(nm)
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
+
 first_image_element = soup.find("img", class_="mw-file-element")
 if first_image_element:
     first_image_url = first_image_element["src"]
     print(first_image_url)
 else:
     print("No image with the class 'mw-file-element' found on the page.")
+
+new = re.sub(r'[0-9]+px', '600px', first_image_url)
+print(new)
 
 
