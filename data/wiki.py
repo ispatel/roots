bac8ceeb41367f70c8b2840a5d44d314be712255
Neelan Patel <neelan@Neelans-MacBook-Pro.local>
2024-02-03 16:36:23 -0500

    added img url

--- data/wiki.py
+++ data/wiki.py
@@ -1,14 +1,30 @@
 import wikipedia
+from bs4 import BeautifulSoup
+import requests
 
 places = ["St. Augustine's University", "John Chavis Memorial Park", "Pope House Museum", "Estey Hall at Shaw University", "African American Civil War Memorial and Museum", "Frederick Douglass National Historic Site", "Martin Luther King, Jr. Memorial", "National Museum of African American History and Culture (NMAAHC)", "Howard Theatre"]
 data = {}
 for x in places:
     data[x] = (wikipedia.page(x).url, wikipedia.summary(x)) 
-    print(x)
-print(data)
+#print(data)
+
 
 def oneQuery(name): 
     return (wikipedia.page(name).url, wikipedia.summary(name)) 
 
+def getURL(name):
+    return wikipedia.page(name).url
+
+nm = "John Chavis Memorial Park"
+url = getURL(nm)
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+first_image_element = soup.find("img", class_="mw-file-element")
+if first_image_element:
+    first_image_url = first_image_element["src"]
+    print(first_image_url)
+else:
+    print("No image with the class 'mw-file-element' found on the page.")
 
 
