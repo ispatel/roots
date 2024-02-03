06e52ff65e68ad7d14327301fc123bb1f2b63b88
ishansp <ispatel@ncsu.edu>
2024-02-03 18:20:55 -0500

    haha lol

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,7 +4,7 @@ import Carousel from "react-bootstrap/Carousel";
 import data from "./test.json";
 import { LoadScript, GoogleMap, Map } from "@react-google-maps/api";
 import StreetMap from "./StreetMap";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 
 const key = process.env.REACT_APP_API_KEY;
 const lib = ["places"];
@@ -27,64 +27,86 @@ function MainCarousel({ city }) {
                         >
                             <div>
                                 <Container>
-                                    <h1
-                                        style={{
-                                            textAlign: "center",
-                                            color: "white",
-                                            fontFamily: "Comic Sans MS",
-                                            fontSize: "7em",
-                                        }}
-                                    >
-                                        {place.name}
-                                    </h1>
-                                    <div
-                                        className="left"
-                                        style={{ padding: "20px" }}
-                                    >
-                                        <img
-                                            src={place.imageUrl}
-                                            alt="African Burial Ground, NYC"
-                                            width={"100%"}
-                                            height={window.innerHeight / 2}
-                                        />
-                                    </div>
-                                </Container>
-                                <Container>
-                                    <div
-                                        className="right"
-                                        style={{ padding: "20px" }}
-                                    >
-                                        <LoadScript
-                                            googleMapsApiKey={key}
-                                            libraries={lib}
+                                    <Row>
+                                        <h1
+                                            style={{
+                                                textAlign: "center",
+                                                color: "white",
+                                                fontFamily: "Comic Sans MS",
+                                                fontSize: "7em",
+                                            }}
                                         >
-                                            <StreetMap
-                                                addr="326 Stonehouse Dr Apex, NC"
-                                                height={window.innerHeight / 2}
-                                                width={"100%"}
-                                            />
-                                        </LoadScript>
-                                    </div>
+                                            {place.name}
+                                        </h1>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <div
+                                                className="left"
+                                                style={{ padding: "20px" }}
+                                            >
+                                                <img
+                                                    src={place.imageUrl}
+                                                    alt="African Burial Ground, NYC"
+                                                    width={"100%"}
+                                                    height={
+                                                        window.innerHeight / 2
+                                                    }
+                                                />
+                                            </div>
+                                        </Col>
+                                        <Col>
+                                            <div
+                                                className="right"
+                                                style={{ padding: "20px" }}
+                                            >
+                                                <LoadScript
+                                                    googleMapsApiKey={key}
+                                                    libraries={lib}
+                                                >
+                                                    <StreetMap
+                                                        addr={`${place.name}, ${place.city}`}
+                                                        height={
+                                                            window.innerHeight /
+                                                            2
+                                                        }
+                                                        width={"100%"}
+                                                    />
+                                                </LoadScript>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                    <Row className="justify-content-md-center text-center">
+                                        <a
+                                            href={place.wikiUrl}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            style={{
+                                                color: "gold",
+                                                fontStyle: "italic",
+                                                fontSize: "1.8em",
+                                                fontFamily: "Arial",
+                                            }}
+                                        >
+                                            Visit Wiki Page
+                                        </a>
+                                    </Row>
+                                    <Row className="text-center">
+                                        <p
+                                            style={{
+                                                color: "white",
+                                                fontFamily: "Arial",
+                                                fontSize: "1.3em",
+                                                padding: "20px",
+                                            }}
+                                        >
+                                            {place.description}
+                                        </p>
+                                    </Row>
                                 </Container>
-                                <a
-                                    href={place.wikiUrl}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    style={{
-                                        color: "gold",
-                                        fontStyle: "italic",
-                                        fontSize: "1.8em",
-                                        fontFamily: "Arial",
-                                    }}
-                                >
-                                    Visit Wiki Page
-                                </a>
-                                <p>{place.description}</p>
                             </div>
                         </div>
-                        <Carousel.Caption>
-                            { }
-                        </Carousel.Caption>
+                        <Carousel.Caption>{}</Carousel.Caption>
                     </Carousel.Item>
                 ))}
         </Carousel>
