d4f2ccf60bef634595f500e99c4944b8ac16a271
svruddar <svrdev26@gmail.com>
2024-02-03 22:32:00 -0500

    adding validation to modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,12 +2,48 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
+
 
 function AddLandmarkModal() {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState('');
+  const [city, setCity] = useState('');
+  const [error, setError] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+    setName('');
+    setCity('');
+    setError(false)
+  };
+
   const handleShow = () => setShow(true);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (name.trim() !== '' && city.trim() !== '') {
+      try {
+        const response = axios.get(`http://localhost:5000/summary?name=${name}&city=${city}`);
+        console.log(response)
+      } catch (error) {
+        setError(true)
+      }
+      
+      
+      console.log('Submitted:', name, city);
+      handleClose();
+    } else {
+      setError(true)
+    }
+  };
 
   return (
     <>
@@ -23,19 +59,29 @@ function AddLandmarkModal() {
           <Form>
             <Form.Group className="mb-3" controlId="form.name">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" autoFocus />
+              <Form.Control
+                type="text"
+                value={name}
+                onChange={handleNameChange}
+                autoFocus
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="form.city">
               <Form.Label>City</Form.Label>
-              <Form.Control type="text" autoFocus/>
+              <Form.Control
+                type="text"
+                value={city}
+                onChange={handleCityChange}
+              />
             </Form.Group>
+            {error && (<div style={{ color: 'red' }} > Submission must be non-empty </div>)}
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleSubmit}>
             Save Changes
           </Button>
         </Modal.Footer>
