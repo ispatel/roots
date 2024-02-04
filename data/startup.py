20cacc5f932ceb913d0b8c2135459aa82fd4fbe0
Neelan Patel <neelan@Neelans-MacBook-Pro.local>
2024-02-03 22:16:26 -0500

    m

--- data/startup.py
+++ data/startup.py
@@ -3,7 +3,7 @@ import requests
 def main():
     index = 0
     num_places_per_city = 4
-    places = ["St. Augustine's University", "John Chavis Memorial Park", "Pope House Museum", "Estey Hall at Shaw University", "African American Civil War Memorial and Museum", "Frederick Douglass National Historic Site", "Martin Luther King, Jr. Memorial", "National Museum of African American History and Culture (NMAAHC)", "Schomburg Center for Research in Black Culture", "African Burial Ground National Monument", "Studio Museum in Harlem", "Apollo Theater", " "]
+    places = ["St. Augustine's University", "John Chavis Memorial Park", "Pope House Museum", "Estey Hall at Shaw University", "African American Civil War Memorial and Museum", "Frederick Douglass National Historic Site", "Martin Luther King, Jr. Memorial", "National Museum of African American History and Culture (NMAAHC)", "Schomburg Center for Research in Black Culture", "African Burial Ground National Monument", "Studio Museum in Harlem", "Apollo Theater Harlem"]
     cities = ["Raleigh", "Washington D.C", "New York City"]
     for city in cities:
         places_in_city = places[index : index + num_places_per_city]
