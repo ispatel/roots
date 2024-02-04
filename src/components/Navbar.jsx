f80a259ba050399e8a22cce1a4190a7ccdf5b24b
ishansp <ispatel@ncsu.edu>
2024-02-03 22:11:27 -0500

    udpated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,8 @@
 import { Container, Dropdown, DropdownButton, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import AddLandmarkModal from "./Modal";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import cityData from "./test.json";
 
 function Navbar({ onCitySelect }) {
     const [city, setCity] = useState(null);
@@ -12,8 +11,6 @@ function Navbar({ onCitySelect }) {
         setCity(city);
         onCitySelect(city);
     };
-
-    const cities = ["New York", "London", "Paris", "Tokyo", "Sydney"];
 
     return (
         <div
@@ -24,37 +21,45 @@ function Navbar({ onCitySelect }) {
                 backgroundColor: "#333",
                 color: "white",
                 padding: "25px",
-                height: "7vh",
+                height: "8vh",
             }}
         >
             <Container>
-            <Row className="align-items-center">
-            <Col xs={4} className="align-items-center">
-            <Container>
-                <DropdownButton
-                    id="dropdown-basic-button"
-                    title={city || "Select a City!"}
-                >
-                    {cities.map((city, index) => {
-                        return (
-                            <Dropdown.Item
-                                key={index}
-                                onClick={() => handleCityChange(city)}
+                <Row className="align-items-center">
+                    <Col xs={4} className="align-items-center">
+                        <Container>
+                            <DropdownButton
+                                id="dropdown-basic-button"
+                                title={city || "Select a City!"}
                             >
-                                {city}
-                            </Dropdown.Item>
-                        )
-                    })}
-                </DropdownButton>
-            </Container>
-            </Col>
-            <Col xs={4} className="align-items-center">
-            <h1>Roots</h1>
-            </Col>
-            <Col xs={4} className="align-items-center">
-            <AddLandmarkModal></AddLandmarkModal>
-            </Col>
-            </Row>
+                                {cityData.map((cityObj, index) => {
+                                    return (
+                                        <Dropdown.Item
+                                            key={index}
+                                            onClick={() =>
+                                                handleCityChange(cityObj.city)
+                                            }
+                                        >
+                                            {cityObj.city}
+                                        </Dropdown.Item>
+                                    );
+                                })}
+                            </DropdownButton>
+                        </Container>
+                    </Col>
+                    <Col
+                        xs={4}
+                        className="align-items-center justify-content-center d-flex"
+                    >
+                        <h1>Roots</h1>
+                    </Col>
+                    <Col
+                        xs={4}
+                        className="d-flex justify-content-end align-items-center"
+                    >
+                        <AddLandmarkModal />
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
