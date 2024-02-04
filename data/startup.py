a1701add1e44d6c287daedf4af891b1a52624c77
Srinath Vasudevan <svasude5@ncsu.edu>
2024-02-03 20:49:15 -0500

    startup start

--- data/startup.py
+++ data/startup.py
@@ -1,5 +1,9 @@
+import requests
+
 def main():
-    url = ''
+    places = ["St. Augustine's University", "John Chavis Memorial Park", "Pope House Museum", "Estey Hall at Shaw University", "African American Civil War Memorial and Museum", "Frederick Douglass National Historic Site", "Martin Luther King, Jr. Memorial", "National Museum of African American History and Culture (NMAAHC)", "Howard Theatre"]
+    for name in places:
+        requests.get(f"localhost:5000/summary?name={name}&city={city}")
 
 if __name__ == "__main__":
     main()
